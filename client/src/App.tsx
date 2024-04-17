9535da05c3df70820260edde9bccab9679c1c559
sameer6699 <sammyjadhav6699@gmail.com>
2024-04-17 18:40:06 +0530

    Initial Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { CssBaseline } from "@mui/material";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
+
+  
+  return (
+    <div className= "app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        hi
+      </ThemeProvider> 
+    </div>
+  )
+}
+export default App;
+ 
